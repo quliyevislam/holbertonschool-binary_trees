8c035a86788634eff5fe3f9f4a9c1adf4e6419cd
Islam <9337@holbertonstudents.com>
2024-08-07 18:51:45 +0400

    Fixed bug by adding `<stdlib.h>` header

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: a pointer to the node to insert the left-child in
