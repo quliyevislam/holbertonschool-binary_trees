220c0ee697bc03e2ae676d56712fcf97060091cb
Islam <9337@holbertonstudents.com>
2024-08-07 18:56:08 +0400

    Fixed memory leak

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (!new_node || !parent)
-		return (NULL);
+		free(new_node), return (NULL);
 
 	new_node->n = value;
 	new_node->parent = NULL;
