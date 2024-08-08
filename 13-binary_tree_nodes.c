6bcfc172328e610d01b39d39b58bd638f1b0c164
Fareedizm <151377488+Fareedizm@users.noreply.github.com>
2024-08-08 22:25:49 +0000

    Fixed betty error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,16 +2,16 @@
 
 /**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
- * @binary_tree_t: a pointer to the root node of
+ * @tree: a pointer to the root node of
  * the tree to count the number of nodes
  *
  * Return: count of the non leave node of 0
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+if (!tree)
+return (0);
 
-	if (tree->left || tree->right)
+if (tree->left || tree->right)
 return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
