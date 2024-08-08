b441f18b7fe2c0e8c2c7f86ee66c73bd46dc694f
Islam <9337@holbertonstudents.com>
2024-08-08 13:38:21 +0000

    Fixed error by sending func pointer

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,6 +11,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	(*func)(tree->n);
-	binary_tree_preorder(tree->left, &print_num);
-	binary_tree_preorder(tree->right, &print_num);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
