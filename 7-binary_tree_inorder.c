bc4ffce92abbf391e70029bec0d8f14cc852ed9d
Islam <9337@holbertonstudents.com>
2024-08-08 13:49:58 +0000

    Fixed error by sending func pointer

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,6 +11,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	(*func)(tree->n);
-	binary_tree_inorder(tree->left, &print_num);
-	binary_tree_inorder(tree->right, &print_num);
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
 }
