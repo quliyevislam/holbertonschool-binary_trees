af2130de4dc7fc53e2696b49c2ce2f9c054137a6
Fareedizm <151377488+Fareedizm@users.noreply.github.com>
2024-08-08 22:23:05 +0000

    Fixed betty error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,5 +13,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left || tree->right)
-		return (1 +binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
