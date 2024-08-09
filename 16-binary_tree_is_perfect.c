4aa073ea536fcd52a2674cece304337b573a15e6
Islam <151377488+Fareedizm@users.noreply.github.com>
2024-08-09 10:57:38 +0000

    Fixed bug by handeling NULL pointer

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,32 +1,32 @@
 #include "binary_trees.h"
-int depth(const binary_tree_t  *node) {
-  int d = 0;
-  while (node != NULL) {
-    d++;
-    node = node->left;
-  }
-  return d;
+
+int depth(const binary_tree_t  *tree)
+{
+int depth = 0;
+while (tree)
+{
+depth++;
+tree = tree->left;
+}
+return depth;
 }
 
-int is_perfect(const binary_tree_t *tree, int d, int level) {
-
-  if (tree == NULL)
-    return (1);
-
- if (tree->left == NULL && tree->right == NULL)
-    return (d == level + 1);
-
-  if (tree->left == NULL || tree->right == NULL)
-    return (0);
-
-  return is_perfect(tree->left, d, level + 1) &&
-       is_perfect(tree->right, d, level + 1);
+int is_perfect(const binary_tree_t *tree, int d, int level)
+{
+if (!tree)
+return (1);
+if (!tree->left && !tree->right)
+return (d == level + 1);
+if (tree->left == NULL || tree->right == NULL)
+return (0);
+return is_perfect(tree->left, d, level + 1) &&
+is_perfect(tree->right, d, level + 1);
 }
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-
-  int d = depth(tree);
-  return is_perfect(tree, d, 0);
+if (!tree)
+return (0);
+int d = depth(tree);
+return is_perfect(tree, d, 0);
 }
 
