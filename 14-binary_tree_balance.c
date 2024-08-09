68c341aab793f3858dc1e1dfd5b5f507bbf053f4
Islam <151377488+Fareedizm@users.noreply.github.com>
2024-08-09 09:25:53 +0000

    Fixed bug by deleting binary_tree_height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,28 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: a pointer to the root node of the tree to measure the height
- *
- * Return: height of the tree or If tree is NULL, your function must return 0
- */
-int binary_tree_height(const binary_tree_t *tree)
-{
-	int left_height, right_height;
-
-	if (!tree)
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-
-	else
-		return (right_height + 1);
-}
-
-/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: a pointer to the root node of the tree to measure the balance factor
  *
@@ -36,8 +14,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	h_left = binary_tree_height(tree->left);
-	h_right = binary_tree_height(tree->right);
+	h_left = (int)binary_tree_height(tree->left);
+	h_right = (int)binary_tree_height(tree->right);
 
 	return (h_left - h_right);
 }
