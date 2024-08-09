29caba5b20c01554603dec60da8a1a490a7c4385
Islam <151377488+Fareedizm@users.noreply.github.com>
2024-08-09 10:08:28 +0000

    Fixed betty erorr

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_is_full - checks if a binary tree is full
  * @tree: a pointer to the root node of the tree to check
  *
- * 
- *
+ * Return: if the tree is full 1 if not 0 or if tree is NULL, 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
