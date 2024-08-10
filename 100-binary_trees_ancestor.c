b734f31e196f0fcb84cd0e778c575aebbd3419ee
Islam <9337@holbertonstudents.com>
2024-08-10 22:26:44 +0400

    Fixed betty erorr

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,25 +1,34 @@
 #include "binary_trees.h"
-
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: a pointer to the first node
+ * @second: a pointer to the second node
+ *
+ * Return: If no common ancestor was found, function returns NULL
+ */
+binary_tree_t*
+binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-
 	const binary_tree_t *a = first;
-    const binary_tree_t *b = second;
+	const binary_tree_t *b = second;
+
 	while (a || b)
-    {
-        if (a == b)
-            return (binary_tree_t *)a;
+	{
+		if (a == b)
+			return ((binary_tree_t *)a);
 
-                if (a)
-            a = a->parent;
-        else
-            a = second;
+		if (a)
+			a = a->parent;
 
-        if (b)
-            b = b->parent;
-        else
-            b = first;
-    }
+		else
+			a = second;
 
-    return NULL;
+		if (b)
+			b = b->parent;
+
+		else
+			b = first;
+	}
+
+	return (NULL);
 }
