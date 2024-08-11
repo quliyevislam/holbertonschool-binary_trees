729405e09c9265fc5ce42a55e72856e4a43d27f0
Islam <9337@holbertonstudents.com>
2024-08-11 12:42:56 +0400

    Fixed betty error

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,10 +1,21 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
+/**
+ * binary_tree_rotate_left - performs a left-rotation on a binary tree
+ * @tree:  a pointer to the root node of the tree to rotate
+ *
+ * Return: a pointer to the new root node of the tree once rotated
+ */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    tree->parent = tree->right;
-    tree->right = tree->right->left;
-    tree->parent->parent = NULL;
-    tree->parent->left = tree;
-    return (tree->parent);
+	if (!tree)
+		return (NULL);
+
+	tree->parent = tree->right;
+	tree->right = tree->right->left;
+	tree->parent->parent = NULL;
+	tree->parent->left = tree;
+
+	return (tree->parent);
 }
